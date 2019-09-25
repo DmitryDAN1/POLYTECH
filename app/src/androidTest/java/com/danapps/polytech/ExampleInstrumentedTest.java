76f05f32c9bcb68a3097e5536d1c52f81d693af9
DmitryDAN <DmitryDAN1@users.noreply.github.com>
2019-09-25 23:29:16 +0300

    Project created

---
+++ app/src/androidTest/java/com/danapps/polytech/ExampleInstrumentedTest.java
@@ -0,0 +1,22 @@
+package com.danapps.polytech;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals("com.danapps.polytech", appContext.getPackageName());
+    }
+}
---
+++ app/src/main/java/com/danapps/polytech/MainActivity.java
@@ -0,0 +1,28 @@
+package com.danapps.polytech;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+
+                return true;
+            }
+        });
+    }
+}
