f8a2f4c6f860c923bbea454e3af679128c44d7e9
DmitryDAN <dmitrydan1@users.noreply.github.com>
2019-10-01 23:29:12 +0300

    Added tasks

--- app/src/main/java/com/danapps/polytech/fragments/tabs/MenuFragment.java
+++ app/src/main/java/com/danapps/polytech/fragments/tabs/MenuFragment.java
@@ -2,6 +2,7 @@ package com.danapps.polytech.fragments.tabs;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
@@ -14,20 +15,28 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.danapps.polytech.R;
+import com.danapps.polytech.activities.auth.AuthActivity;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class MenuFragment extends Fragment {
 
+    @SuppressLint("SetTextI18n")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         @SuppressLint("InflateParams") View view = inflater.inflate(R.layout.fragment_menu, null);
 
-        SharedPreferences sPref = getActivity().getSharedPreferences("UserInfo", Context.MODE_PRIVATE);
+        SharedPreferences sPref = Objects.requireNonNull(getActivity()).getSharedPreferences("UserInfo", Context.MODE_PRIVATE);
 
         TextView profileBlockCurrentGroupTV = view.findViewById(R.id.menu_profileBlock_currentGroupTV);
         TextView settingsBlockCurrentGroupTV = view.findViewById(R.id.menu_settingsBlock_currentGroupTV);
         profileBlockCurrentGroupTV.setText(sPref.getString("UserGroup", ""));
         settingsBlockCurrentGroupTV.setText(sPref.getString("UserGroup", ""));
+
+        TextView  profileBlockName = view.findViewById(R.id.profileBlock_name);
+        profileBlockName.setText(sPref.getString("UserName", "Name") + " " + sPref.getString("UserSurname", "Surname"));
 
 
         view.findViewById(R.id.profileBlock).setOnClickListener(new View.OnClickListener() {
@@ -41,6 +50,14 @@ public class MenuFragment extends Fragment {
             @Override
             public void onClick(View v) {
 
+            }
+        });
+
+        view.findViewById(R.id.exitBlock_exitBTN).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getActivity(), AuthActivity.class));
             }
         });
 
