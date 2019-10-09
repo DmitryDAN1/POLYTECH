f0ded20d9f640cbd81d2479acaeb0b1641877eab
DmitryDAN <dmitrydan1@users.noreply.github.com>
2019-10-09 23:55:12 +0300

    Fixed a bug with the constant display of an error when creating a new note

--- app/src/main/java/com/danapps/polytech/fragments/tabs/NotesFragment.java
+++ app/src/main/java/com/danapps/polytech/fragments/tabs/NotesFragment.java
@@ -26,11 +26,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class NotesFragment extends Fragment {
+public class NotesFragment extends Fragment{
 
     private RecyclerView recyclerView;
-    private RecyclerView.Adapter adapter;
-    private List<NoteListItem> listItems;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -44,42 +42,36 @@ public class NotesFragment extends Fragment {
 
         UpdateRV(recyclerView, sPref);
 
-        view.findViewById(R.id.notes_notes_addBTN).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                BottomSheetDialog dialogB = new BottomSheetDialog(Objects.requireNonNull(getActivity()));
-                LayoutInflater inflater1 = LayoutInflater.from(getContext());
-                @SuppressLint("InflateParams") View window_add_note = inflater1.inflate(R.layout.window_add_note, null);
-                dialogB.setContentView(window_add_note);
-                dialogB.show();
+        view.findViewById(R.id.notes_notes_addBTN).setOnClickListener(v -> {
+            BottomSheetDialog dialogB = new BottomSheetDialog(Objects.requireNonNull(getActivity()));
+            LayoutInflater inflater1 = LayoutInflater.from(getContext());
+            @SuppressLint("InflateParams") View window_add_note = inflater1.inflate(R.layout.window_add_note, null);
+            dialogB.setContentView(window_add_note);
+            dialogB.show();
 
-                TextView errorTV = window_add_note.findViewById(R.id.notes_errorTV);
-                EditText titleET = window_add_note.findViewById(R.id.notes_titleET);
-                EditText subtitleET = window_add_note.findViewById(R.id.notes_subtitleET);
+            TextView errorTV = window_add_note.findViewById(R.id.notes_errorTV);
+            EditText titleET = window_add_note.findViewById(R.id.notes_titleET);
+            EditText subtitleET = window_add_note.findViewById(R.id.notes_subtitleET);
 
-                window_add_note.findViewById(R.id.notes_saveBTN).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if (titleET.getText().toString().isEmpty() && subtitleET.getText().toString().isEmpty())
-                            dialogB.hide();
-                        else {
-                            if (!titleET.getText().toString().isEmpty() && subtitleET.getText().toString().isEmpty())
-                                errorTV.setText(getString(R.string.notes_error_subtitle));
-                            else if (titleET.getText().toString().isEmpty() && !subtitleET.getText().toString().isEmpty())
-                                errorTV.setText(getString(R.string.notes_error_title));
-                            else {
-                                int NotesCount = sPref.getInt("NotesCount", 0);
-                                sPref.edit().putString(("Title" + String.valueOf(NotesCount + 1)), titleET.getText().toString()).apply();
-                                sPref.edit().putString(("Subtitle" + String.valueOf(NotesCount + 1)), subtitleET.getText().toString()).apply();
-                                sPref.edit().putInt("NotesCount", (NotesCount + 1)).apply();
-                                UpdateRV(recyclerView, sPref);
-                                dialogB.hide();
-                            }
-                        }
+            window_add_note.findViewById(R.id.notes_saveBTN).setOnClickListener(v1 -> {
+                if (titleET.getText().toString().isEmpty() && subtitleET.getText().toString().isEmpty())
+                    dialogB.hide();
+                else {
+                    if (!titleET.getText().toString().isEmpty() && subtitleET.getText().toString().isEmpty())
+                        errorTV.setText(getString(R.string.notes_error_subtitle));
+                    else if (titleET.getText().toString().isEmpty() && !subtitleET.getText().toString().isEmpty())
+                        errorTV.setText(getString(R.string.notes_error_title));
+                    else {
+                        int NotesCount = sPref.getInt("NotesCount", 0);
+                        sPref.edit().putString(("Title" + (NotesCount + 1)), titleET.getText().toString()).apply();
+                        sPref.edit().putString(("Subtitle" + (NotesCount + 1)), subtitleET.getText().toString()).apply();
+                        sPref.edit().putInt("NotesCount", (NotesCount + 1)).apply();
+                        UpdateRV(recyclerView, sPref);
+                        dialogB.hide();
                     }
-                });
+                }
+            });
 
-            }
         });
 
 
@@ -88,7 +80,7 @@ public class NotesFragment extends Fragment {
 
     private void UpdateRV (RecyclerView recyclerView, SharedPreferences sPref) {
         int NotesCount = sPref.getInt("NotesCount", 0);
-        listItems = new ArrayList<>();
+        List<NoteListItem> listItems = new ArrayList<>();
 
         for (int i = 1; i <= NotesCount; i++) {
             NoteListItem listItem = new NoteListItem(
@@ -100,7 +92,7 @@ public class NotesFragment extends Fragment {
             listItems.add(listItem);
         }
 
-        adapter = new NoteAdapter(listItems, getContext());
+        RecyclerView.Adapter adapter = new NoteAdapter(listItems, getContext());
         recyclerView.setAdapter(adapter);
     }
 
