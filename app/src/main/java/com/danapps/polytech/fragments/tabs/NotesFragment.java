20692a16505f689f46a7fc0f7f1aad193450df9a
DmitryDAN <dmitrydan1@users.noreply.github.com>
2019-10-11 02:47:23 +0300

    Added ability to delete notes with swipe left

--- app/src/main/java/com/danapps/polytech/fragments/tabs/NotesFragment.java
+++ app/src/main/java/com/danapps/polytech/fragments/tabs/NotesFragment.java
@@ -3,14 +3,19 @@ package com.danapps.polytech.fragments.tabs;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +24,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.danapps.polytech.R;
+import com.danapps.polytech.activities.MainActivity;
 import com.danapps.polytech.notes.NoteAdapter;
 import com.danapps.polytech.notes.NoteListItem;
 import com.danapps.polytech.notes.RecyclerTouchListener;
@@ -38,13 +44,53 @@ public class NotesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_notes, container, false);
-
         SharedPreferences sPref = Objects.requireNonNull(getActivity()).getSharedPreferences("NotesInfo", Context.MODE_PRIVATE);
-
         recyclerView = view.findViewById(R.id.notes_recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
+        ItemTouchHelper.Callback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Objects.requireNonNull(getContext()));
+
+                builder.setTitle("Удаление заметки")
+                        .setMessage("Вы уверены, что хотите удалить заметку?")
+                        .setNegativeButton(getString(R.string.No), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                UpdateRV(recyclerView, sPref);
+                            }
+                        })
+                        .setPositiveButton(getString(R.string.Yes), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                int noteNumber = viewHolder.getAdapterPosition() + 1;
+                                int notesCount = sPref.getInt("NotesCount", 0);
+
+                                for (int j = noteNumber; j < notesCount; j++) {
+                                    sPref.edit().putString("Title" + j, sPref.getString("Title" + (j + 1), "None")).apply();
+                                    sPref.edit().putString("Subtitle" + j, sPref.getString("Subtitle" + (j + 1), "None")).apply();
+                                }
+
+                                sPref.edit().remove("Title" + notesCount).remove("Subtitle" + notesCount).putInt("NotesCount", notesCount - 1).apply();
+
+                                UpdateRV(recyclerView, sPref);
+                            }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        };
+
+
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(recyclerView);
 
         UpdateRV(recyclerView, sPref);
 
@@ -95,6 +141,11 @@ public class NotesFragment extends Fragment {
                 Toast.makeText(getContext(), String.valueOf(position+10), Toast.LENGTH_SHORT).show();
             }
         }));
+
+
+
+
+
 
 
         return view;
--- app/src/main/java/com/danapps/polytech/notes/NoteAdapter.java
+++ app/src/main/java/com/danapps/polytech/notes/NoteAdapter.java
@@ -1,25 +1,15 @@
 package com.danapps.polytech.notes;
 
-import android.app.LauncherActivity;
 import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.provider.ContactsContract;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.view.menu.MenuView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.danapps.polytech.R;
-import com.danapps.polytech.activities.StartActivity;
-import com.danapps.polytech.fragments.tabs.NoteEditFragment;
 
 import java.util.List;
 
