acedbac5138a49b8647173b13151fc7cdc9c0b0e
DmitryDAN <dmitrydan1@users.noreply.github.com>
2019-11-05 03:30:57 +0300

    Fixed bug on registration, when email EditText is empty

--- app/src/main/java/com/danapps/polytech/fragments/tabs/RegisterEmailFragment.java
+++ app/src/main/java/com/danapps/polytech/fragments/tabs/RegisterEmailFragment.java
@@ -21,7 +21,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterEmailFragment extends Fragment {
 
-    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -30,22 +30,26 @@ public class RegisterEmailFragment extends Fragment {
         view.findViewById(R.id.reg_email_backBTN).setOnClickListener(v ->
                 ((MainActivity) getActivity()).loadFragment(7));
 
-        view.findViewById(R.id.reg_email_nextBTN).setOnClickListener(v -> {
-            EditText emailET = view.findViewById(R.id.reg_email_emailET);
+        if (((EditText) view.findViewById(R.id.reg_email_emailET)).getText().toString().isEmpty())
+            ((TextInputLayout) view.findViewById(R.id.reg_email_emailTIL)).setError(getString(R.string.email_error));
+        else {
+            view.findViewById(R.id.reg_email_nextBTN).setOnClickListener(v -> {
+                EditText emailET = view.findViewById(R.id.reg_email_emailET);
                 mAuth.fetchSignInMethodsForEmail(emailET.getText().toString())
-                    .addOnSuccessListener(command -> {
-                        Log.d("RegisterEmail:", "SignInMethods:" + command.getSignInMethods().size());
-                        if (command.getSignInMethods().size() > 0)
-                            ((TextInputLayout) view.findViewById(R.id.reg_email_emailTIL)).setError(getString(R.string.email_error_registered));
-                        else {
-                            SharedPreferences tPref = getActivity().getSharedPreferences("TimedInfo", Context.MODE_PRIVATE);
-                            tPref.edit().putString("TimedEmail", emailET.getText().toString()).apply();
-                            ((MainActivity) getActivity()).loadFragment(9);
-                        }
+                        .addOnSuccessListener(command -> {
+                            Log.d("RegisterEmail:", "SignInMethods:" + command.getSignInMethods().size());
+                            if (command.getSignInMethods().size() > 0)
+                                ((TextInputLayout) view.findViewById(R.id.reg_email_emailTIL)).setError(getString(R.string.email_error_registered));
+                            else {
+                                SharedPreferences tPref = getActivity().getSharedPreferences("TimedInfo", Context.MODE_PRIVATE);
+                                tPref.edit().putString("TimedEmail", emailET.getText().toString()).apply();
+                                ((MainActivity) getActivity()).loadFragment(9);
+                            }
 
-                    }).addOnFailureListener(e1 -> Snackbar.make(view, getString(R.string.auth_main_error), Snackbar.LENGTH_SHORT).show());
-
-        });
+                        })
+                        .addOnFailureListener(e1 -> Snackbar.make(view, getString(R.string.auth_main_error), Snackbar.LENGTH_SHORT).show());
+            });
+        }
         return view;
     }
 
