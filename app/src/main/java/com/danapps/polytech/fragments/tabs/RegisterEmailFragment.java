3ace971a188ea959b73454d0bcb8234fc764a4f3
DmitryDAN <dmitrydan1@users.noreply.github.com>
2019-11-05 03:37:12 +0300

    Registration bug fixed

--- app/src/main/java/com/danapps/polytech/fragments/tabs/RegisterEmailFragment.java
+++ app/src/main/java/com/danapps/polytech/fragments/tabs/RegisterEmailFragment.java
@@ -30,26 +30,27 @@ public class RegisterEmailFragment extends Fragment {
         view.findViewById(R.id.reg_email_backBTN).setOnClickListener(v ->
                 ((MainActivity) getActivity()).loadFragment(7));
 
-        if (((EditText) view.findViewById(R.id.reg_email_emailET)).getText().toString().isEmpty())
-            ((TextInputLayout) view.findViewById(R.id.reg_email_emailTIL)).setError(getString(R.string.email_error));
-        else {
-            view.findViewById(R.id.reg_email_nextBTN).setOnClickListener(v -> {
-                EditText emailET = view.findViewById(R.id.reg_email_emailET);
-                mAuth.fetchSignInMethodsForEmail(emailET.getText().toString())
-                        .addOnSuccessListener(command -> {
-                            Log.d("RegisterEmail:", "SignInMethods:" + command.getSignInMethods().size());
-                            if (command.getSignInMethods().size() > 0)
-                                ((TextInputLayout) view.findViewById(R.id.reg_email_emailTIL)).setError(getString(R.string.email_error_registered));
-                            else {
-                                SharedPreferences tPref = getActivity().getSharedPreferences("TimedInfo", Context.MODE_PRIVATE);
-                                tPref.edit().putString("TimedEmail", emailET.getText().toString()).apply();
-                                ((MainActivity) getActivity()).loadFragment(9);
-                            }
 
-                        })
-                        .addOnFailureListener(e1 -> Snackbar.make(view, getString(R.string.auth_main_error), Snackbar.LENGTH_SHORT).show());
+            view.findViewById(R.id.reg_email_nextBTN).setOnClickListener(v -> {
+                if (((EditText) view.findViewById(R.id.reg_email_emailET)).getText().toString().isEmpty())
+                    ((TextInputLayout) view.findViewById(R.id.reg_email_emailTIL)).setError(getString(R.string.email_error));
+                else {
+                    EditText emailET = view.findViewById(R.id.reg_email_emailET);
+                    mAuth.fetchSignInMethodsForEmail(emailET.getText().toString())
+                            .addOnSuccessListener(command -> {
+                                Log.d("RegisterEmail:", "SignInMethods:" + command.getSignInMethods().size());
+                                if (command.getSignInMethods().size() > 0)
+                                    ((TextInputLayout) view.findViewById(R.id.reg_email_emailTIL)).setError(getString(R.string.email_error_registered));
+                                else {
+                                    SharedPreferences tPref = getActivity().getSharedPreferences("TimedInfo", Context.MODE_PRIVATE);
+                                    tPref.edit().putString("TimedEmail", emailET.getText().toString()).apply();
+                                    ((MainActivity) getActivity()).loadFragment(9);
+                                }
+
+                            })
+                            .addOnFailureListener(e1 -> Snackbar.make(view, getString(R.string.reg_finish_error), Snackbar.LENGTH_SHORT).show());
+                }
             });
-        }
         return view;
     }
 
